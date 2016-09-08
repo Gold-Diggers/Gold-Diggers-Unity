7b1166b875731761065eeb7c812d2d28204d4311
nlzz22 <nlzz_22@hotmail.com>
2016-09-08 14:28:39 +0800

    Basic implementation

---
+++ Assets/Scripts/PlayerBaseController.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PlayerBaseController : MonoBehaviour {
+    public float speed;
+    public float jumpHeight;
+    private Rigidbody2D rb2d;
+    private float yPos;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        yPos = transform.position.y;
+    }
+
+    void updateYPos()
+    {
+        yPos = transform.position.y;
+    }
+
+    void FixedUpdate()
+    {
+        handleJump();
+        handleDig();
+        handleHorizontalMovement();
+        updateYPos(); // must be last
+    }
+
+    void handleJump()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (isCharacterOnPlatform())
+            { // normal jump
+                rb2d.AddForce(new Vector2(0, 100) * jumpHeight);
+            } else if (isCharacterFalling())
+            { // hover
+                rb2d.AddForce(new Vector2(0, 3) * jumpHeight);
+            }
+        }
+    }
+
+    // Dig might dig 2 blocks instead of 1 in order to allow player to fall through.
+    void handleDig()
+    {
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (isCharacterOnPlatform())
+            {
+                float currX = transform.position.x;
+                float currY = transform.position.y;
+                Vector2 ptA = new Vector2((float)(currX - 0.25), currY);
+                Vector2 ptB = new Vector2((float)(currX + 0.25), currY - 1);
+                Collider2D[] col = Physics2D.OverlapAreaAll(ptA, ptB, 1<<8);
+                /*if (col.Length == 1)
+                {
+                    print("1");
+                    Destroy(col[0].gameObject);
+                } else if (col.Length == 2)
+                {
+                    print("2");
+                    float distA = Math.Abs(col[0].transform.position.x - currX);
+                    float distB = Math.Abs(col[1].transform.position.x - currX);
+                    if (distA < distB)
+                    {
+                        transform.position = transform.position - new Vector3(distA, 0);
+                        Destroy(col[0].gameObject);
+                    } else
+                    {
+                        transform.position = transform.position + new Vector3(distB, 0);
+                        Destroy(col[1].gameObject);
+                    }
+                } else
+                {
+                    // should not reach here.
+                    print("im here");
+                }*/
+                foreach (Collider2D current in col)
+                {
+                    if (current.transform.tag == "Platform")
+                    {
+                        Destroy(current.gameObject);
+                    }
+                }
+            }
+        }
+    }
+
+    bool isCharacterOnPlatform()
+    {
+        //if (transform.position.y == yPos)
+        if (Math.Abs(transform.position.y - yPos) < 0.001)
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+    }
+
+    bool isCharacterFalling()
+    {
+        if (transform.position.y < yPos)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void handleHorizontalMovement()
+    {
+        // The commented out code is buggy - might get stuck
+        /*float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);*/
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+    }
+}
