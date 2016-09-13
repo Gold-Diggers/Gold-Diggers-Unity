5772bc9aa52094d6bd134690cf36fd793eb2c6d0
nlzz22 <nlzz_22@hotmail.com>
2016-09-13 20:48:43 +0800

    Prevent user from moving horizontally when digging

--- Assets/Scripts/PlayerBaseController.cs
+++ Assets/Scripts/PlayerBaseController.cs
@@ -9,11 +9,16 @@ public class PlayerBaseController : MonoBehaviour {
     private const double Y_VELOCITY_THRESHOLD = -2.00000000;
     private Rigidbody2D rb2d;
     private float yPos;
+    // For handling period when player is digging
+    private bool isDigging;
+    private int diggingCounter;
 
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         yPos = transform.position.y;
+        isDigging = false;
+        diggingCounter = 0;
     }
 
     void updateYPos()
@@ -26,6 +31,7 @@ public class PlayerBaseController : MonoBehaviour {
         handleJump();
         handleDig();
         handleHorizontalMovement();
+        handleDigCooldown();
         updateYPos(); // must be last
     }
 
@@ -47,6 +53,24 @@ public class PlayerBaseController : MonoBehaviour {
         }
     }
 
+    private void handleDigCooldown()
+    {
+        if (isDigging)
+        {
+            diggingCounter--;
+            if (diggingCounter == 0)
+            {
+                isDigging = false;
+            }
+        }
+    }
+
+    private void updateDigging()
+    {
+        isDigging = true;
+        diggingCounter = 5;
+    }
+
     // Dig might dig 2 blocks instead of 1 in order to allow player to fall through.
     void handleDig()
     {
@@ -54,6 +78,7 @@ public class PlayerBaseController : MonoBehaviour {
         {
             if (isCharacterOnPlatform())
             {
+                updateDigging();
                 float currX = transform.position.x;
                 float currY = transform.position.y;
                 Vector2 ptA = new Vector2((float)(currX - 0.25), currY);
@@ -123,14 +148,16 @@ public class PlayerBaseController : MonoBehaviour {
         /*float moveHorizontal = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(moveHorizontal, 0);
         rb2d.AddForce(movement * speed);*/
-
-        if (Input.GetKey(KeyCode.D))
+        if (!isDigging)
         {
-            transform.position += Vector3.right * speed * Time.deltaTime;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += Vector3.left * speed * Time.deltaTime;
-        }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+        } 
     }
 }
