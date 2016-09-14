cdaa51102434cca3249af21d26aa4f5b882f2229
nlzz22 <nlzz_22@hotmail.com>
2016-09-14 13:40:09 +0800

    Fixed camera view to within background

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
     public GameObject player;
     private Vector3 cameraOffset;
-
+    private const float CAMERA_BOUNDS = 1.52f;
+    
 	// Use this for initialization
 	void Start () {
         cameraOffset = transform.position - player.transform.position;
@@ -13,6 +14,11 @@ public class CameraController : MonoBehaviour {
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + cameraOffset;
-    }
+        float yPos = player.transform.position.y + cameraOffset.y;
+        float xPos = Mathf.Clamp(player.transform.position.x + cameraOffset.x, -CAMERA_BOUNDS , CAMERA_BOUNDS);
+        float zPos = player.transform.position.z + cameraOffset.z;
+        transform.position = new Vector3(xPos, yPos, zPos);
+
+        //transform.position = player.transform.position + cameraOffset;
+    } 
 }
