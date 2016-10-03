258736653885bbceb787c927ed93eb6e899d3191
nlzz22 <nlzz_22@hotmail.com>
2016-10-03 22:44:15 +0800

    Fix Assertion error bug when colliding with background boundary

--- Assets/Scripts/PlayerCollisionController.cs
+++ Assets/Scripts/PlayerCollisionController.cs
@@ -24,6 +24,7 @@ public class PlayerCollisionController : MonoBehaviour {
     private const string SPECIAL_DIAMOND = "SpecialDiamond";
     private const string TREASURE_CHEST = "TreasureChest";
     private const string SPECIAL_TREASURE_CHEST = "SpecialTreasureChest";
+    private const string BG_BOUNDARY = "BackgroundBoundary";
     private const string ERROR_INVALID_LIVES_VALUE = "ERROR: 'lives' attribute cannot be < 0.";
     private const string ERROR_INVALID_INVINCIBILITY_VALUE = "ERROR: 'invincibility' attribute cannot be < 0.";
     private const string ERROR_INVALID_SPECIAL_DIAMOND_VALUE = "ERROR: 'specialDiamonds' attribute cannot be > 3.";
@@ -81,9 +82,10 @@ public class PlayerCollisionController : MonoBehaviour {
     void OnCollisionStay2D(Collision2D coll)
     {
         string collidedObject = coll.gameObject.tag;
+        // if (!Equals(collidedObject, PLATFORM)) print(collidedObject);
         Assert.IsTrue(Equals(collidedObject, PLATFORM) || Equals(collidedObject, DIAMOND) || Equals(collidedObject, SPECIAL_DIAMOND) ||
                       Equals(collidedObject, TREASURE_CHEST) || Equals(collidedObject, SPECIAL_TREASURE_CHEST) || Equals(collidedObject, MONSTER) ||
-                      Equals(collidedObject, TRAP), ERROR_UNEXPECTED_COLLISION_EVENT);
+                      Equals(collidedObject, TRAP) || Equals(collidedObject, BG_BOUNDARY), ERROR_UNEXPECTED_COLLISION_EVENT);
 
         switch (collidedObject)
         {
@@ -91,7 +93,7 @@ public class PlayerCollisionController : MonoBehaviour {
                 // this case is already handled in the control functions
                 break;
 
-            case "BackgroundBoundary":
+            case BG_BOUNDARY:
                 // no action needed here
                 break;
 
