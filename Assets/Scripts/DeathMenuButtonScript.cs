b40e327d6df66b388b26f1ec9de09ee8747d2e6f
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2016-10-11 23:12:18 +0800

    Added menu controller scripts

---
+++ Assets/Scripts/DeathMenuButtonScript.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DeathMenuButtonScript : MonoBehaviour {
+
+    private const int DEFAULT_SAVED_LEVEL = 1;
+    private int savedLevel;
+
+    public Image screenImage;
+    public Sprite defaultScreen;
+    public Sprite replayHoverScreen;
+    public Sprite quitHoverScreen;
+
+    public int SavedLevel
+    {
+        get
+        {
+            return savedLevel;
+        }
+        set
+        {
+            savedLevel = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        savedLevel = DEFAULT_SAVED_LEVEL;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnReplayHover()
+    {
+        screenImage.sprite = replayHoverScreen;
+    }
+
+    public void OnQuitHover()
+    {
+        screenImage.sprite = quitHoverScreen;
+    }
+
+    public void OnHoverLeave()
+    {
+        screenImage.sprite = defaultScreen;
+    }
+
+    public void Replay()
+    {
+        string levelName = "Level " + SavedLevel;
+        print("Loading " + levelName + " ...");
+        SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+    }
+
+    public void Quit()
+    {
+        print("Leaving to main menu...");
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/MainMenuButtonScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuButtonScript : MonoBehaviour {
+
+    public Image screenImage;
+    public Sprite defaultScreen;
+    public Sprite startHoverScreen;
+    public Sprite tutorialHoverScreen;
+    public Canvas tutorialCanvas;
+
+    // Use this for initialization
+    void Start () {
+        tutorialCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown("escape"))
+        {
+            tutorialCanvas.enabled = false;
+        }
+	}
+
+    public void OnStartHover()
+    {
+        screenImage.sprite = startHoverScreen;
+    }
+
+    public void OnTutorialHover()
+    {
+        screenImage.sprite = tutorialHoverScreen;
+    }
+
+    public void OnHoverLeave()
+    {
+        screenImage.sprite = defaultScreen;
+    }
+
+    public void Play()
+    {
+        print("Loading Level 1...");
+        SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+    }
+
+    public void OpenTutorial()
+    {
+        print("Opening tutorial screen...");
+        tutorialCanvas.enabled = true;
+    }
+}
