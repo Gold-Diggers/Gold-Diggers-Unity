12b90c3e7c13408832b12f456fcf8c631ae64ed8
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2016-10-13 20:01:52 +0800

    Added end-game scene scripting

---
+++ Assets/Scripts/TransitionControllerScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TransitionControllerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
