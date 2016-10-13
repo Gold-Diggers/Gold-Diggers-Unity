774c151e7cb953b1440d6e45a859cdb7545fe9ab
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2016-10-13 19:34:12 +0800

    Added loading text placeholder

--- Assets/Scripts/MainMenuButtonScript.cs
+++ Assets/Scripts/MainMenuButtonScript.cs
@@ -11,27 +11,50 @@ public class MainMenuButtonScript : MonoBehaviour {
     public Sprite tutorialHoverScreen;
     public Canvas tutorialCanvas;
     public Text prompt;
+    public Text loadPrompt;
     private float originalRed;
     private float originalGreen;
     private float originalBlue;
+    private AsyncOperation loadOp;
 
     private float speed = 2f;
 
     // Use this for initialization
     void Start () {
         tutorialCanvas.enabled = false;
+        loadPrompt.enabled = false;
         originalRed = prompt.color.r;
         originalGreen = prompt.color.g;
         originalBlue = prompt.color.b;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKeyDown("escape"))
+	void Update ()
+    {
+        openTutorialOnEscapeKey();
+        triggerTextFadeEffect();
+    }
+
+    private void handleLoadOperation()
+    {
+        if (loadOp != null)
+        {
+            if (loadOp.isDone) SceneManager.UnloadScene("MainMenu");
+        }
+    }
+
+    private void openTutorialOnEscapeKey()
+    {
+        if (Input.GetKeyDown("escape"))
         {
             tutorialCanvas.enabled = false;
         }
+    }
+
+    private void triggerTextFadeEffect()
+    {
         prompt.color = new Color(originalRed, originalGreen, originalBlue, (Mathf.Sin(Time.time * speed) + 1.0f) / 2.0f);
+        loadPrompt.color = new Color(originalRed, originalGreen, originalBlue, (Mathf.Sin(Time.time * speed) + 1.0f) / 2.0f);
     }
 
     public void OnStartHover()
@@ -51,14 +74,15 @@ public class MainMenuButtonScript : MonoBehaviour {
 
     public void Play()
     {
+        loadPrompt.enabled = true;
         print("Loading Level 1...");
-        SceneManager.LoadScene("Level 1", LoadSceneMode.Single);
+        AsyncOperation loadOp = SceneManager.LoadSceneAsync("Level 1", LoadSceneMode.Single);
+        loadOp.allowSceneActivation = true;
     }
 
     public void OpenTutorial()
     {
         //print("Opening tutorial screen...");
-        prompt.color = new Color(originalRed, originalGreen, originalBlue, 1.0f);
         tutorialCanvas.enabled = true;
     }
 }
