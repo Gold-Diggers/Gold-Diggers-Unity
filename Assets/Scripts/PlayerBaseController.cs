67458a0b6cb24685f6a94248d571effeed342e52
nlzz22 <nlzz_22@hotmail.com>
2016-10-18 14:35:47 +0800

    lower monster speed and revised jump values

--- Assets/Scripts/PlayerBaseController.cs
+++ Assets/Scripts/PlayerBaseController.cs
@@ -11,8 +11,8 @@ public class PlayerBaseController : MonoBehaviour {
     private const double Y_VELOCITY_THRESHOLD = -2.0;
     private const float HORIZONTAL_COLLISION_THRESHOLD_ENEMIES = 0.1f;
     private const float HORIZONTAL_COLLISION_THRESHOLD_PLATFORM = 0.3f;
-    private const float JUMP_FORCE = 65f;
-    private const float HOVER_FORCE = 3.5f;
+    private const float JUMP_FORCE = 67.5f;
+    private const float HOVER_FORCE = 2.0f; // for jetpack: 3.5f
     private const float CHAR_ON_PLATFORM_Y_DIFF_THRESHOLD = 0.001f;
 
     /* ================= Player physics attributes ================= */
