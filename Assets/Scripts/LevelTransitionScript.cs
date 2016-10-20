99196d37d903b4222defe0c5416276b2975081e4
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2016-10-20 13:28:55 +0800

    Added level transition script for transition stages

---
+++ Assets/Scripts/LevelTransitionScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelTransitionScript : MonoBehaviour {
+
+    private AsyncOperation loadOp;
+
+	// Use this for initialization
+	void Start () {
+        string levelName = "Level " + GlobalPlayerScript.Instance.level;
+        print("Loading " + levelName);
+        loadOp = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Single);
+        loadOp.allowSceneActivation = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            loadOp.allowSceneActivation = true;
+        }
+    }
+}
