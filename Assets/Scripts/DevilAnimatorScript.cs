bd5e63ad346081034ad93b1d841befb8b44d37fe
Tay Guo Qiang <tayguoqiang1993@gmail.com>
2016-10-24 20:08:58 +0800

    Added level 2 npc animation

---
+++ Assets/Scripts/DevilAnimatorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DevilAnimatorScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Animate();
+	}
+
+    void Animate()
+    {
+        Vector3 movement = new Vector3(transform.position.x, transform.position.y + 0.005f * Mathf.Sin(Time.time * 2), 0);
+        transform.position = movement;
+    }
+}
