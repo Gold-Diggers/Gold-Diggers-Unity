3873325fb8c67e4859ae0e8f6bbb9d98358cdac9
nlzz22 <nlzz_22@hotmail.com>
2016-10-30 23:10:57 +0800

    Implemented transitions from level 3 to endings, note ending 5 and 6 are dummies

--- Assets/Scripts/LevelThreeEndScript.cs
+++ Assets/Scripts/LevelThreeEndScript.cs
@@ -5,12 +5,16 @@ using UnityEngine.SceneManagement;
 public class LevelThreeEndScript : MonoBehaviour {
     private bool isFading;
     private GameObject player;
+    private float startTime;
+    private float timeTaken;
 
     private const int DIAMOND_THRESHOLD = 175;
+    private const float TIME_THRESHOLD = 50f;
 
     void Start()
     {
         isFading = false;
+        startTime = Time.time;
     }
 
     void Update()
@@ -25,6 +29,7 @@ public class LevelThreeEndScript : MonoBehaviour {
     {
         if (!isFading && other.gameObject.name == "Player")
         {
+            timeTaken = Time.time - startTime;
             isFading = true;
             player = other.gameObject;
         }
@@ -47,6 +52,7 @@ public class LevelThreeEndScript : MonoBehaviour {
     {
         int specialDiamond = player.GetComponent<PlayerCollisionController>().specialDiamonds;
         int diamond = player.GetComponent<PlayerCollisionController>().diamonds;
+        print(timeTaken);
 
         if (specialDiamond == 3)
         { // Golden Ending
@@ -54,8 +60,11 @@ public class LevelThreeEndScript : MonoBehaviour {
         } else if (diamond >= DIAMOND_THRESHOLD)
         { // Diamond Collector
             SceneManager.LoadScene("Ending 4", LoadSceneMode.Single);
+        } else if (timeTaken <= TIME_THRESHOLD)
+        { // Hero Ending
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); // dummy
         } else
-        {
+        { // Steve Ending
             SceneManager.LoadScene("MainMenu", LoadSceneMode.Single); // dummy
         }
     }
